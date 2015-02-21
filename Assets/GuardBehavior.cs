43247c4ef208e57db1ff02b16114a55550bf44aa
jshiraef <joeshiraef@gmail.com>
2015-02-21 02:25:23 -0500

    split Kristin's prototype map into tiles. began player behaviour

---
+++ Assets/GuardBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardBehavior : MonoBehaviour {
+
+	public Transform sightStart; sightEnd;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		raycasting ();
+		behaviours ();
+	}
+
+	void raycasting()
+	{
+
+	}
+
+	void behaviours()
+	{
+
+	}
+}
---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Movement();
+	}
+
+	void Movement()
+	{
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			transform.Translate (Vector2.right * 4f * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			transform.Translate (-Vector2.right * 4f * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.S)) 
+		{
+			transform.Translate (-Vector2.up * 4f * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.W)) 
+		{
+			transform.Translate (Vector2.up * 4f * Time.deltaTime);
+		}
+	}
+}
