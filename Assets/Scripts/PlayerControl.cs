319099e63bc81f8cbb3b6132548d6049dbe570ab
jshiraef <joeshiraef@gmail.com>
2015-02-22 14:06:09 -0500

    messing around with linecasting/raycasting collision concepts

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class PlayerControl : MonoBehaviour {
 
 	public bool interact = false;
-	public Transform lineStart, lineEnd;
+	public bool grounded = false;
+	public Transform lineStart, lineEnd, groundedEnd;
+
+	RaycastHit2D whatIHit;
 
 	// Use this for initialization
 	void Start ()
@@ -22,7 +25,24 @@ public class PlayerControl : MonoBehaviour {
 	void Raycasting()
 	{
 		Debug.DrawLine (lineStart.position, lineEnd.position, Color.green);
+		Debug.DrawLine (this.transform.position, groundedEnd.position, Color.green);
 
+		grounded = Physics2D.Linecast (this.transform.position, groundedEnd.position, 1 << LayerMask.NameToLayer ("ground"));
+
+		if(Physics2D.Linecast (lineStart.position, lineEnd.position, 1 << LayerMask.NameToLayer ("Guard")))
+        {
+			whatIHit = Physics2D.Linecast (lineStart.position, lineEnd.position, 1 << LayerMask.NameToLayer ("Guard"));
+			interact = true;
+		}
+		else
+		{
+			interact = false;
+		}
+
+		if(Input.GetKeyDown (KeyCode.E) && interact == true)
+		 {
+			Destroy (whatIHit.collider.gameObject);
+		 }
 	}
 
 	void Movement()
@@ -45,6 +65,11 @@ public class PlayerControl : MonoBehaviour {
 		if (Input.GetKey (KeyCode.W)) 
 		{
 			transform.Translate (Vector2.up * 4f * Time.deltaTime);
+		}
+
+		if(Input.GetKeyDown (KeyCode.Space) && grounded == true)
+		{
+			rigidbody2D.AddForce(Vector2.up * 200f);
 		}
 	}
 }
