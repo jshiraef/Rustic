5002c0614b723802bcfe4eb7dd9d9f7b42875be3
jshiraef <joeshiraef@gmail.com>
2015-03-21 06:58:29 -0400

    added debug gui script for viewing controller Axes during play also messed around with animations and shrank the spritesheets into smaller images for better performance

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -13,6 +13,7 @@ public class PlayerControl : MonoBehaviour {
 
 	public Direction direction;
 
+
 	Animator anim;
 
 	// Use this for initialization
@@ -56,12 +57,10 @@ public class PlayerControl : MonoBehaviour {
 
 	void Movement()
 	{
-
 		anim.SetFloat ("speed", Mathf.Abs (Input.GetAxis ("Vertical")));
 		anim.SetFloat ("speed", Mathf.Abs (Input.GetAxis ("Horizontal")));
 
-		if (Input.GetAxisRaw ("Horizontal") > 0) 
-		{
+		if (Input.GetAxisRaw ("Horizontal") > 0) {
 			anim.SetBool ("runReleased", false);
 			this.direction = Direction.EAST;
 
@@ -70,10 +69,9 @@ public class PlayerControl : MonoBehaviour {
 
 		}
 
-		if (Input.GetAxisRaw("Horizontal") < 0) 
-		{
+		if (Input.GetAxisRaw ("Horizontal") < 0) {
 			anim.SetBool ("runReleased", false);
-			this.direction = Direction.WEST;
+//			this.direction = Direction.WEST;
 
 			transform.Translate (-Vector2.right * 3f * Time.deltaTime);
 //			transform.eulerAngles = new Vector2(0, 180);  // this sets the rotation of the gamebject
@@ -87,42 +85,97 @@ public class PlayerControl : MonoBehaviour {
 			transform.Translate (-Vector2.up * 3f * Time.deltaTime);
 		}
 
-		if (Input.GetAxisRaw ("Vertical") > 0) 
-		{
+		if (Input.GetAxisRaw ("Vertical") > 0) {
 			anim.SetBool ("runReleased", false);
 			this.direction = Direction.NORTH;
 
 			transform.Translate (Vector2.up * 3f * Time.deltaTime);
 		}
 
-		if (Input.GetAxisRaw ("Horizontal") < 0 && Input.GetAxis ("Vertical") > .2f)
+		if (Input.GetAxisRaw("Vertical") < 0 && Input.GetAxisRaw("Horizontal") < 0) 
 		{
-			this.direction = Direction.NORTHWEST1;
+			this.direction = Direction.SOUTHWEST250;
 		}
+
+//		Debug.Log ("the Horizontal GetAxis is: " + Input.GetAxis ("Horizontal"));
+
+		if (this.direction == Direction.SOUTH && Input.GetKeyUp (KeyCode.A) && Input.GetAxis ("Horizontal") < 0 && Input.GetAxis ("Horizontal") >= -0.2) 
+		{
+			Debug.Log ("this happened");
+			this.direction = Direction.SOUTHWEST250;
+		}
+
+		if (this.direction == Direction.SOUTH && Input.GetKeyUp (KeyCode.A) && Input.GetAxis ("Horizontal") < -0.2 && Input.GetAxis ("Horizontal") >= -0.4) 
+		{
+			Debug.Log ("SouthWest 230 degrees");
+			this.direction = Direction.SOUTHWEST230;
+		}
+
+		if (this.direction == Direction.SOUTH && Input.GetKeyUp (KeyCode.A) && Input.GetAxis ("Horizontal") < -0.4 && Input.GetAxis ("Horizontal") >= -0.7)  
+		{
+			Debug.Log ("SouthWest 210 degrees");
+			this.direction = Direction.SOUTHWEST210;
+		}
+
+
+
+//		if ((Input.GetAxisRaw ("Horizontal") < 0 && Input.GetAxis ("Vertical") > .7f) || (Input.GetAxis("Vertical") > 0f && Input.GetAxis ("Horizontal") > -.8f && Input.GetAxis("Horizontal") < -.2f))
+//		{
+//			this.direction = Direction.NORTHWEST1;
+//		}
+//
+//		if(Input.GetAxis ("Horizontal") < 0f && Input.GetAxis ("Vertical") < 0.8f && Input.GetAxis("Vertical") > 0.3f)
+//		{
+//			this.direction = Direction.NORTHWEST2;
+//		}
+
+
 
 		if(Input.GetKeyDown (KeyCode.Space) && grounded == true)
 		{
 			rigidbody2D.AddForce(Vector2.up * 200f);
 		}
 
-		Debug.Log ("the player's direction is: " + this.direction);
-		Debug.Log ("the vertical axis input is " + Input.GetAxis ("Vertical"));
+//		Debug.Log ("the player's direction is: " + this.direction);
+//		Debug.Log ("the vertical axis input is " + Input.GetAxis ("Vertical"));
+//		Debug.Log ("the horizontal axis input is " + Input.GetAxis ("Horizontal"));
+//		Debug.Log ("the direction is " + this.direction);
 
 	}
 
 	void animationSetter()
 	{
-		if (this.direction == Direction.NORTH)
+		if(this.direction == Direction.SOUTHEAST330)
+		{
+			anim.SetInteger("Direction", 16);
+		}
+
+		if(this.direction == Direction.SOUTHEAST310)
+		{
+			anim.SetInteger("Direction", 15);
+		}
+
+		if(this.direction == Direction.SOUTHEAST290)
+		{
+			anim.SetInteger("Direction", 14);
+		}
+
+		if(this.direction == Direction.SOUTH)
+		{
+			anim.SetInteger("Direction", 13);
+		}
+
+		if (this.direction == Direction.SOUTHWEST250)
 		{
 			anim.SetInteger ("Direction", 12);
 		}
 
-		if (this.direction == Direction.NORTHWEST1)
+		if (this.direction == Direction.SOUTHWEST230)
 		{
 			anim.SetInteger ("Direction", 11);
 		}
 
-		if (this.direction == Direction.NORTHWEST2)
+		if (this.direction == Direction.SOUTHWEST210)
 		{
 			anim.SetInteger ("Direction", 10);
 		}
@@ -132,47 +185,47 @@ public class PlayerControl : MonoBehaviour {
 			anim.SetInteger ("Direction", 9);
 		}
 
-		if (this.direction == Direction.SOUTHWEST1)
+		if (this.direction == Direction.NORTHWEST150)
 		{
 			anim.SetInteger ("Direction", 8);
 		}
 
-		if (this.direction == Direction.SOUTHWEST2)
+		if (this.direction == Direction.NORTHWEST130)
 		{
 			anim.SetInteger ("Direction", 7);
 		}
 
-		if (this.direction == Direction.SOUTH)
+		if (this.direction == Direction.NORTHWEST110)
 		{
 			anim.SetInteger ("Direction", 6);
 		}
 
-		if (this.direction == Direction.SOUTHEAST1)
+		if (this.direction == Direction.NORTH)
 		{
 			anim.SetInteger ("Direction", 5);
 		}
 
-		if (this.direction == Direction.SOUTHEAST2)
+		if (this.direction == Direction.NORTHEAST70)
 		{
 			anim.SetInteger ("Direction", 4);
 		}
 
-		if (this.direction == Direction.EAST)
+		if (this.direction == Direction.NORTHEAST50)
 		{
 			anim.SetInteger ("Direction", 3);
 		}
 
-		if (this.direction == Direction.NORTHEAST1)
+		if (this.direction == Direction.NORTHEAST30)
 		{
 			anim.SetInteger ("Direction", 2);
 		}
 
-		if (this.direction == Direction.NORTHEAST2)
+		if (this.direction == Direction.EAST)
 		{
 			anim.SetInteger ("Direction", 1);
 		}
 
-
+//		Debug.Log ("the animator's direction int is" + anim.GetInteger ("Direction"));
 
 
 		if (Input.GetKeyUp (KeyCode.W) || Input.GetKeyUp (KeyCode.S) || Input.GetKeyUp (KeyCode.D) || Input.GetKeyUp (KeyCode.A))
@@ -185,7 +238,7 @@ public class PlayerControl : MonoBehaviour {
 
 	public enum Direction
 	{
-		NORTH, SOUTH, EAST, WEST, NORTHEAST1, NORTHEAST2, NORTHWEST1, NORTHWEST2, SOUTHEAST1, SOUTHEAST2, SOUTHWEST1, SOUTHWEST2, NULL
+		NORTH, SOUTH, EAST, WEST, NORTHEAST30, NORTHEAST50, NORTHEAST70, NORTHWEST110, NORTHWEST130, NORTHWEST150, SOUTHEAST290, SOUTHEAST310, SOUTHEAST330, SOUTHWEST210, SOUTHWEST230, SOUTHWEST250, NULL
 	}
 	
 }
