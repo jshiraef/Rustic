503791a4d17b704dc1d8e261af0b1888c48cf89e
jshiraef <joeshiraef@gmail.com>
2018-01-26 22:54:20 -0800

    testing detailed scene; renderMask script added

---
+++ Assets/RenderMask.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RenderMask : MonoBehaviour {
+
+    Animator MovingMask;
+    private bool grassTrigger;
+    private bool waterTrigger;
+
+    private GameObject renderMask;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        MovingMask = GetComponent<Animator>();
+        renderMask = GameObject.Find("RenderMask");
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {   
+        if(waterTrigger)
+        {
+            MovingMask.Play("waterFlowMask");
+        }
+
+        if(grassTrigger)
+        {
+            
+        }
+
+        if (GetComponentInParent<PlayerControl>().isIdle)
+        {
+            MovingMask.enabled = false;
+        }
+        else MovingMask.enabled = true;
+
+
+    }
+}
