bbcb2dc9d887e179e80dda4068ab58e3ae4f7086
Joseph Shiraef <Joseph Shiraef>
2018-02-11 22:05:38 -0800

    edited the particle emmitter for the bubble trail

--- Assets/Scripts/smartBubbleTrail.cs
+++ Assets/Scripts/smartBubbleTrail.cs
@@ -5,19 +5,21 @@ public class smartBubbleTrail : MonoBehaviour {
 
     public GameObject bubbleTrail;
     private ParticleSystem bubbleEmitter;
+    private GameObject player;
 
     // Use this for initialization
     void Start () {
 
         bubbleTrail = GameObject.Find("bubbleTrail");
         bubbleEmitter = bubbleTrail.GetComponent<ParticleSystem>();
+        player = GameObject.Find("player");
 
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        if (GetComponentInParent<PlayerControl>().inWater)
+        if (player.GetComponent<PlayerControl>().inWater)
         {
             if(!bubbleEmitter.isPlaying)
             {
