d979bd73f13b32339fdaa6c5e3fb20af2917196f
jshiraef <joeshiraef@gmail.com>
2018-03-26 02:53:02 -0700

    added line detection for depth sorting layer script

--- Assets/Scripts/SortingOrderScript.cs
+++ Assets/Scripts/SortingOrderScript.cs
@@ -14,6 +14,16 @@ public class SortingOrderScript : MonoBehaviour
 	// will change the nearby objects sorting layer
 	public double threshold = 9.5;
 
+    // Here we create two empty gameobjects which represent two points. 
+    // A line is therefore created for sorting depth rendering based on the player's position relative to this line
+    public GameObject thresholdPoint1;
+    public GameObject thresholdPoint2;
+
+    private Vector2 threshold1;
+    private Vector2 threshold2;
+    private float slope;
+    private float yintercept;
+
 
 	// Use this for initialization
 	void Start () 
@@ -22,22 +32,61 @@ public class SortingOrderScript : MonoBehaviour
 		player = GameObject.Find ("player");
 
 		currentLayerName = sprite.sortingLayerName.ToString ();
-	}
+
+        if (thresholdPoint1 != null)
+        {
+            threshold1 = new Vector2(thresholdPoint1.transform.position.x, thresholdPoint1.transform.position.y);
+
+        }
+
+        if (thresholdPoint2 != null)
+        {
+            threshold2 = new Vector2(thresholdPoint2.transform.position.x, thresholdPoint2.transform.position.y);
+        }
+
+        if (thresholdPoint1 != null && thresholdPoint2 != null)
+        {
+            slope = (thresholdPoint2.transform.position.y - thresholdPoint1.transform.position.y) /
+                (thresholdPoint2.transform.position.x - thresholdPoint1.transform.position.x);
+
+            yintercept = thresholdPoint2.transform.position.y - (slope * thresholdPoint2.transform.position.x);
+        }
+
+    }
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (player.transform.position.y > threshold) 
+        if (thresholdPoint1 != null && thresholdPoint2 != null)
+        {
+            slope = (thresholdPoint2.transform.position.y - thresholdPoint1.transform.position.y) /
+                (thresholdPoint2.transform.position.x - thresholdPoint1.transform.position.x);
+
+            yintercept = thresholdPoint2.transform.position.y - (slope * thresholdPoint2.transform.position.x);
+        }
+
+        if (player.transform.position.y > threshold) 
 		{
 //			sprite.sortingOrder = sortingOrder;
 			sprite.sortingLayerName = OverlapLayer;
-		} 
-		else
-			sprite.sortingLayerName = currentLayerName;
+		}
+        else if (player.transform.position.y > (slope * player.transform.position.x) + yintercept)
+        {
+            sprite.sortingLayerName = OverlapLayer;
+        }
+        else
+            sprite.sortingLayerName = currentLayerName;
 
-//		Debug.Log ("the sprite's current sorting layer is" + sprite.sortingLayerName);
-////		Debug.Log ("the player's x & y are" + player.transform.position.x + " , " + player.transform.position.y);
-//        Debug.Log ("the threshold is " + threshold);
-//        Debug.Log("the player's y is" + player.transform.position.y);
-	}
+        if (thresholdPoint1 != null && thresholdPoint2 != null)
+        {
+            Debug.Log("the slope is " + slope);
+            Debug.Log("the thresholdPoint2 is " + thresholdPoint2.transform.position.x + " , " + thresholdPoint2.transform.position.y);
+            Debug.Log("the yintercept is " + yintercept);
+        }
+
+        //		Debug.Log ("the sprite's current sorting layer is" + sprite.sortingLayerName);
+        ////		Debug.Log ("the player's x & y are" + player.transform.position.x + " , " + player.transform.position.y);
+        //        Debug.Log ("the threshold is " + threshold);
+        //        Debug.Log("the player's y is" + player.transform.position.y);
+    }
 }
