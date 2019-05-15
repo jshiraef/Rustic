fc4c39cbe00e40f29308dc182d31404112ec7988
jshiraef <joeshiraef@gmail.com>
2019-05-15 13:54:35 -0700

    added an upgrade flare for visual realization of upgrade

---
+++ Assets/Scripts/Misc/UpgradeFlare.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeFlare : MonoBehaviour
+{
+
+    public bool flareTrigger;
+    public float flareSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flareSpeed = .1f;
+        this.transform.localPosition = new Vector3(0, 0, -12f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            flareTrigger = true;
+        }
+
+        if (flareTrigger)
+        {
+            this.transform.Translate(0, 0, flareSpeed);
+        }
+
+        if(this.transform.position.z >= 10)
+        {
+            flareTrigger = false;
+            this.transform.localPosition = new Vector3(0, 0, -12f);
+        }
+    }
+}
