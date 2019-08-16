90119faedd7fbda2558b91890a29964ef517292f
jshiraef <joeshiraef@gmail.com>
2019-08-16 12:36:15 -0700

    added dogwood tree .psb and animation

--- Assets/Scripts/Misc/waterBob.cs
+++ Assets/Scripts/Misc/waterBob.cs
@@ -11,17 +11,38 @@ public class waterBob : MonoBehaviour
     float period = 1;
 
     private Vector3 initialPosition;
+    private Vector3 initialRotation;
     private float offset;
+
+    public bool Vertical;
+
+    public bool rotationOnly;
 
     private void Awake()
     {
         initialPosition = transform.position;
+        initialRotation = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z);
 
         offset = 1 - (Random.value * 2);
     }
 
     private void Update()
     {
-        transform.position = initialPosition - Vector3.left * Mathf.Sin((Time.time + offset) * period) * height;
+        if (!rotationOnly)
+        {
+            if (!Vertical)
+            {
+                transform.position = initialPosition - Vector3.left * Mathf.Sin((Time.time + offset) * period) * height;
+            }
+            else
+            {
+                transform.position = initialPosition - Vector3.up * Mathf.Sin((Time.time + offset) * period) * height;
+            }
+        }
+        else
+        {
+            transform.Rotate(0,0,Mathf.Sin((Time.time + offset) * period) * height, Space.Self);
+        }
+
     }
 }
