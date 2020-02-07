384bfa7ce1a132af02d2be7cb8019ea4ad4b1559
jshiraef <joeshiraef@gmail.com>
2020-02-07 02:34:48 -0800

    flowers surrounding floral house are moving

---
+++ Assets/SpriteOutline/Editor/SpriteOutlineImporter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class SpriteOutlineImporter : AssetPostprocessor {
+
+	void OnPreprocessTexture() {
+		TextureImporter importer = (TextureImporter)assetImporter;
+
+		if (!assetPath.Contains (SpriteOutline2.IMAGE_EXT))
+			return;
+
+		importer.filterMode          = FilterMode.Point;
+		importer.alphaIsTransparency = false;
+	}
+
+}
